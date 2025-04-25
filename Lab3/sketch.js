375b5a432f863a3371eab8c8f3ee4c20a52cccbf
abwi03 <abueno@ocadu.ca>
2025-04-24 21:40:00 -0400

    lab 3 done

--- Lab3/sketch.js
+++ Lab3/sketch.js
@@ -1,12 +1,63 @@
+let colorPicker;
+let bgColorPicker;
+let strokeWeightSlider;
+let bgColorButton;
+let eraserButton;
+let fillCanvasButton;
+let erasing = false;
 
+function setup() {
+    createCanvas(1080, 500);
 
+    colorPicker = createColorPicker('DarkSlateBlue');
+    colorPicker.position(400, height + 10);
 
-function setup(){
-    createCanvas(1080,500);
-    background('mistyrose');
-    strokeWeight(10)
+    strokeWeightSlider = createSlider(6, 20, 10, 2);
+    strokeWeightSlider.position(458, height + 15);
+
+    bgColorPicker = createColorPicker('Lavender');
+    bgColorPicker.position(600, height + 10);
+
+    bgColorButton = createButton('RESET');
+    bgColorButton.position(300, height + 15);
+    bgColorButton.mousePressed(repaint);
+
+    eraserButton = createButton('ERASER');
+    eraserButton.position(685, height + 15);
+    eraserButton.mousePressed(toggleEraser);
+
+    fillCanvasButton = createButton('FILL CANVAS');
+    fillCanvasButton.position(750, height + 10);
+    fillCanvasButton.mousePressed(fillCanvas);
+    fillCanvasButton.position(480, height + 45);
+
+    background(bgColorPicker.value());
 }
 
-function draw(){
-    line(mouseX, mouseY, pmouseX, pmouseY);
+function draw() {
+    if (mouseIsPressed && mouseY <= height) {
+        strokeWeight(strokeWeightSlider.value());
+
+        if (erasing) {
+            stroke(bgColorPicker.value());
+        } else {
+            stroke(colorPicker.value());
+        }
+
+        line(mouseX, mouseY, pmouseX, pmouseY);
+    }
 }
+
+function repaint() {
+    background(bgColorPicker.value());
+}
+
+function toggleEraser() {
+    erasing = !erasing;
+    eraserButton.html(erasing ? 'DRAW' : 'ERASER');
+}
+
+function fillCanvas() {
+    background(bgColorPicker.value());
+}
+
